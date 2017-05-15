e5366c20a9928c0b08b87acb809eed28cfebdbdc
Andrew Gardner <andga82@ippo.itn.liu.se>
2017-05-15 14:03:00 +0200

    These settings are helpful when distributing.

--- monkeys/settings.py
+++ monkeys/settings.py
@@ -118,12 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 # TODO: If I want to point at an app, can I do it without hard-coding the app path?
 LOGIN_REDIRECT_URL = '/'

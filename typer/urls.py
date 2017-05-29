5a9c9b432cd401f1839a990dc59b6211529a61f4
Andrew Gardner <andrew.gardner@liu.se>
2017-05-29 09:38:44 -0400

    Die names can have hyphens, numbers, and underscores.

--- typer/urls.py
+++ typer/urls.py
@@ -5,7 +5,7 @@ from .views import indexView, summaryHomeView, summaryView
 app_name = 'typer'
 urlpatterns = [
     #url(r'^$', views.index, name='index'),
-    url(r'^(?P<dieName>[a-zA-Z]+)/$', indexView, name='index'),
-    url(r'^(?P<dieName>[a-zA-Z]+)/summary/$', summaryHomeView, name='summaryHome'),
-    url(r'^(?P<dieName>[a-zA-Z]+)/summary/(?P<imageId>[0-9]+)/$', summaryView, name='summaryView'),
+    url(r'^(?P<dieName>[a-zA-Z0-9-_]+)/$', indexView, name='index'),
+    url(r'^(?P<dieName>[a-zA-Z0-9-_]+)/summary/$', summaryHomeView, name='summaryHome'),
+    url(r'^(?P<dieName>[a-zA-Z0-9-_]+)/summary/(?P<imageId>[0-9]+)/$', summaryView, name='summaryView'),
 ]

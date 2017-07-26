3097d39b93c3e928ce15d7bf89bffd2b3ad6e758
Andrew Gardner <andrew.gardner@liu.se>
2017-07-26 15:13:57 +0200

    Cleaned up the regex for user name url generation.

--- typer/urls.py
+++ typer/urls.py
@@ -5,7 +5,7 @@ from .views import indexView, dieInstructionsView, adminSummaryHomeView, adminSu
 app_name = 'typer'
 urlpatterns = [
     url(r'^(?P<dieName>[a-zA-Z0-9-_]+)/$', indexView, name='index'),
-    url(r'^(?P<dieName>[a-zA-Z0-9-_]+)/statistics/(?P<userName>[a-zA-Z0-9-_.@]+)/$', dieSpecificUserStatisticsView, name='dieSpecificUserStatistics'),
+    url(r'^(?P<dieName>[a-zA-Z0-9-_]+)/statistics/(?P<userName>[a-zA-Z0-9\._@\-]+)/$', dieSpecificUserStatisticsView, name='dieSpecificUserStatistics'),
     url(r'^(?P<dieName>[a-zA-Z0-9-_]+)/instructions/$', dieInstructionsView, name='dieInstructions'),
     url(r'^(?P<dieName>[a-zA-Z0-9-_]+)/adminStatistics/$', adminStatisticsView, name='adminStatistics'),
     url(r'^(?P<dieName>[a-zA-Z0-9-_]+)/adminSummary/$', adminSummaryHomeView, name='adminSummaryHome'),

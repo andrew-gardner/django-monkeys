9a35c440e591971cec8190554648f254b7280c67
John McMaster <JohnDMcMaster@gmail.com>
2018-02-09 23:29:20 -0800

    db2txt: export cv data

--- tools/db2txt.py
+++ tools/db2txt.py
@@ -12,6 +12,7 @@ from typer.models import TypedDie
 import argparse
 import ast
 import re
+import json
 
 from PIL import Image, ImageDraw
 
@@ -20,8 +21,7 @@ Each are 8x8
 '''
 warnings = []
 warnings_field = {}
-def romwf(rom, gcol, grow, fieldm, feedback):
-    overrides = feedback
+def romwf(rom, gcol, grow, fieldm, overrides):
     absrow = grow * 8
     abscol = gcol * 8
     entries = sum(fieldm.values())
@@ -189,7 +189,10 @@ def isimg(td, img_want):
     return str(td.dieImage.image).find(img_want) == 0
 
 def get_dist(img_want):
-    '''Get typed bits distribution per task'''
+    '''
+    Get typed bits distribution per field
+    Get a distribution for a position via ret[(gcol, grow)][(fcol, frow)]
+    '''
     dist = {}
     matches = 0
     for tdi, td in enumerate(TypedDie.objects.all()):
@@ -227,6 +230,19 @@ def get_dist(img_want):
         raise ValueError()
     return dist
 
+def dist_by_bits(dist):
+    '''Return a per bit instead of per solution dist'''
+    ret = {}
+    for solution, frequency in dist.iteritems():
+        fi = 0
+        for frow in xrange(8):
+            for fcol in xrange(8):
+                pos_freqs = ret.setdefault((fcol, frow), {})
+                bitchar = solution[fi]
+                pos_freqs[bitchar] = pos_freqs.setdefault(bitchar, 0) + frequency
+                fi += 1
+    return ret
+
 def im_roi(icol, irow):
     '''Get an ROI for just one bit in a task image'''
     if icol >= 8 or irow >= 8:
@@ -257,9 +273,82 @@ def save_txt(rom, f):
             f.write('\n')
     return ret
 
-def run(img_name, feedback=None, fn_out=None):
-    if not feedback:
-        feedback = {}
+# XXX: too many open files without ulimit
+# maybe change to gen_images
+# Load images
+load_images_cache = {}
+def load_images(img_name, toload):
+    for (gcol, grow) in toload:
+        load_images_cache[(gcol, grow)] = get_image(img_name, gcol, grow)
+    return load_images_cache
+
+def get_image(img_name, gcol, grow):
+    fn = 'static/%s/%s_%02d_%02d.png' % (img_name, img_name, gcol, grow)
+    return Image.open(fn)
+
+def gen_warning_images(out_dir, img_name):
+    print 'Generating warning images'
+    images = load_images(img_name, warnings)
+
+    # Mask
+    #print images.keys()
+    #print warnings_field.keys()
+    for (gcol, grow), field in warnings_field.iteritems():
+        im = images[(gcol, grow)]
+        d = ImageDraw.Draw(im)
+        for frow in xrange(8):
+            for fcol in xrange(8):
+                c = field[frow * 8 + fcol]
+                if c not in '01':
+                    roi = im_roi(fcol, frow)
+                    d.rectangle(roi, fill=None, outline=(0xFF, 0xFF, 0))
+
+    # Save
+    for (gcol, grow), im in images.iteritems():
+        im.save(os.path.join(out_dir, '%02d_%02d_m.png' % (gcol, grow)))
+
+def gen_cv_images(meta_dir, img_name, dist, rom):
+    print 'Generating CV images'
+    #images = load_images(img_name, dist)
+
+    meta_bits = {}
+    for (gcol, grow), field in dist.iteritems():
+        field_bits = dist_by_bits(field)
+        #im = images[(gcol, grow)]
+        im = get_image(img_name, gcol, grow)
+        for frow in xrange(8):
+            arow = grow * 8 + frow
+            for fcol in xrange(8):
+                acol = gcol * 8 + fcol
+                roi = im_roi(fcol, frow)
+                if meta_dir:
+                    bitfn = "%02dgc-%02dgr_%02dfc-%02dfr.png" % (gcol, grow, fcol, frow)
+                    meta_bit = {
+                        # Global absolute coordinates
+                        'col': acol,
+                        'row': arow,
+                        # Global tile coordinates
+                        'gcol': gcol,
+                        'grow': grow,
+                        # Field (within a tile) coordinates
+                        'frow': frow,
+                        'fcol': fcol,
+                        # Answer frequency distribution like
+                        # {'0': 3, '1': 1, '?': 1}
+                        "dist": field_bits[(fcol, frow)],
+                        "best": romr(rom, acol, arow),
+                        }
+                    #print meta_bit
+                    imc = im.crop(roi)
+                    imc.save(os.path.join(meta_dir, "bit", bitfn))
+                    imc.close()
+                    meta_bits[bitfn] = meta_bit
+        im.close()
+    return meta_bits
+
+def run(img_name, overrides=None, fn_out=None, meta_dir=None):
+    if not overrides:
+        overrides = {}
 
     # Reassemble the ROM
     print 'Assemble test'
@@ -272,8 +361,26 @@ def run(img_name, feedback=None, fn_out=None):
     dist = get_dist(img_name)
     print ' done'
 
+    if meta_dir:
+        if not os.path.exists(meta_dir):
+            os.mkdir(meta_dir)
+        if not os.path.exists(meta_dir + '/bit'):
+            os.mkdir(meta_dir + '/bit')
+    meta = {
+        "meta": {
+            "source": "django-monkeys/db2txt",
+            "override": str(overrides),
+            "img": img_name,
+        },
+        #"bit": meta_bits
+    }
+
     for (col, row), fields in sorted(dist.iteritems()):
-        romwf(rom, col, row, fields, feedback=feedback)
+        romwf(rom, col, row, fields, overrides=overrides)
+
+    # warning images trashes, run first
+    if meta_dir:
+        meta['bit'] = gen_cv_images(meta_dir, img_name, dist, rom)
 
     if warnings:
         print 'Warning template (%d)' % len(warnings)
@@ -289,31 +396,7 @@ def run(img_name, feedback=None, fn_out=None):
         out_dir = 'db2bin/%s' % img_name
         os.path.exists(out_dir) or os.mkdir(out_dir)
 
-        def gen_images():
-            # Load images
-            images = {}
-            for (gcol, grow) in warnings:
-                fn = 'static/%s/%s_%02d_%02d.png' % (img_name, img_name, gcol, grow)
-                im = Image.open(fn)
-                images[(gcol, grow)] = im
-
-            # Mask
-            #print images.keys()
-            #print warnings_field.keys()
-            for (gcol, grow), field in warnings_field.iteritems():
-                im = images[(gcol, grow)]
-                d = ImageDraw.Draw(im)
-                for frow in xrange(8):
-                    for fcol in xrange(8):
-                        c = field[frow * 8 + fcol]
-                        if c not in '01':
-                            roi = im_roi(fcol, frow)
-                            d.rectangle(roi, fill=None, outline=(0xFF, 0xFF, 0))
-
-            # Save
-            for (gcol, grow), im in images.iteritems():
-                im.save(os.path.join(out_dir, '%02d_%02d_m.png' % (gcol, grow)))
-        gen_images()
+        gen_warning_images(out_dir, img_name)
 
         raise Exception("Resolve warnings before continuing")
 
@@ -322,15 +405,20 @@ def run(img_name, feedback=None, fn_out=None):
     print 'Writing %s' % fn_out
     save_txt(rom, open(fn_out, 'w'))
 
+    if meta_dir:
+        json.dump(meta, open(os.path.join(meta_dir, 'meta.json'), 'w'),
+                  sort_keys=True, indent=4, separators=(',', ': '))
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract ROM .bin from monkey DB')
     parser.add_argument('--verbose', '-v', action='store_true', help='verbose')
     parser.add_argument('--cf', '-c', help='Correction file')
+    parser.add_argument('--meta', '-m', help='Output CV metadata training file to given directory')
     parser.add_argument('image', help='Image file to process (ex: sega_315-5571_xpol)')
     parser.add_argument('fn_out', nargs='?', default=None, help='Output file name')
     args = parser.parse_args()
 
-    feedback = None
+    overrides = None
     if args.cf:
-        feedback = ast.literal_eval(open(args.cf, 'r').read())
-    run(img_name=args.image, feedback=feedback, fn_out=args.fn_out)
+        overrides = ast.literal_eval(open(args.cf, 'r').read())
+    run(img_name=args.image, overrides=overrides, fn_out=args.fn_out, meta_dir=args.meta)
